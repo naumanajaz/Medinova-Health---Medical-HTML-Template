352cbfa4643cbb869b91011344120d3df6054979
naumanajaz <https://github.com/naumanajaz/REAL-ESTATE-and-Builders.git>
2022-09-06 10:52:58 +0500

    I have done some changes

--- js/style.js
+++ js/style.js
@@ -1,4 +1,17 @@
 function show(){
-    let box = document.getElementById('box')
-    box.style.transform = 'translateX(0)'
+    let box = document.getElementById('box');
+    if (box.style.transform == 'translateX(-350px)') {
+        box.style.transform = 'translateX(0)';
+    } else {
+        box.style.transform = 'translateX(-350px)';
+    }
+}
+function myfunction(){
+    let x = document.querySelector('.elms');
+    console.log(elms.style.display = 'block')
+    elms.style.display = 'block';
+}
+function myhide(){
+    let y = document.querySelector('.elms');
+    elms.style.display = 'none';
 }
